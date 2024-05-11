0fa6a242955048040fcd52557d8a53d45d2e23d2
Amith-Abey-Stephen <amithabeystephen@gmail.com>
2024-05-11 08:29:51 +0530

    nav a tag

--- main.js
+++ main.js
@@ -1,12 +1,11 @@
 let course = document.getElementById("cor");
 
-course.onclick = function(){
-    if(course.innerText === "Course")
-        {
-            course.textContent = "Home";
-        }
-    else{
+course.onclick = function() {
+    if (course.innerText === "Course") {
+        course.textContent = "Home";
+        course.href = "#"; 
+    } else {
         course.textContent = "Course";
-        }
+        course.href = "#course"; 
+    }
 }
-
