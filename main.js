500e31b17673e80dd61ca2c02b171d21c2bc4bf1
Amith-Abey-Stephen <amithabeystephen@gmail.com>
2024-05-22 17:58:08 +0530

    minior adjustments in home and login

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ let course = document.getElementById("cor");
 course.onclick = function() {
     if (course.innerText === "Course") {
         course.textContent = "Home";
-        course.href = "#course"; 
+        course.href = "#1"; 
     } else {
         course.textContent = "Course";
         course.href = "#"; 
