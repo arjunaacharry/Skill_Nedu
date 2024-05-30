c97830be1b87db5bfc0b29dbd832949cc01bfe20
Amendamaria <amendamaria005@gmail.com>
2024-05-30 19:57:32 +0530

    Aligned successfull

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ let course = document.getElementById("cor");
 course.onclick = function() {
     if (course.innerText === "Course") {
         course.textContent = "Home";
-        course.href = "#1"; 
+        course.href = "#4"; 
     } else {
         course.textContent = "Course";
         course.href = "#"; 
