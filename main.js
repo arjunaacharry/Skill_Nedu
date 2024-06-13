1619604a7f3e460aa4eadb152101026b6e4371d2
Amith-Abey-Stephen <amithabeystephen@gmail.com>
2024-06-13 15:56:52 +0530

    nav toggle

--- main.js
+++ main.js
@@ -11,3 +11,16 @@ course.onclick = function() {
 }
 // 
 
+
+let navbarToggler = document.getElementById("navbar-toggler");
+let navbarMenu = document.getElementById("navbar-menu");
+
+navbarToggler.addEventListener("click", function() {
+    if (navbarMenu.style.display === "flex") {
+        navbarMenu.style.display = "none";
+        navbarToggler.classList.remove("open");
+    } else {
+        navbarMenu.style.display = "flex";
+        navbarToggler.classList.add("open");
+    }
+});
